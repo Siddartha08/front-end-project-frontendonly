8f19085fe9c72fb48c14bf6ee2cdab7b7feca4c2
unknown <jemery89@gmail.com>
2019-10-27 17:04:45 -0500

    modified links and title

--- src/components/note.js
+++ src/components/note.js
@@ -13,8 +13,8 @@ console.log(props)
 
         return (
             <div>
-            <h1>{props.notes[props.notes._id] == props.notes[props.params.id] ? props.notes.title : 'loading title'}</h1>
-            <div>{props.notes[props.notes._id] == props.notes[props.params.id] ? props.notes.content : 'loading content'}</div>
+            <h1>{props.notes[props.notes._id] == props.notes[props.params._id] ? props.notes.title : 'loading title'}</h1>
+            <div>{props.notes[props.notes._id] == props.notes[props.params._id] ? props.notes.content : 'loading content'}</div>
             <Link  to={`/edit/${props.match.params._id}`} >
             <button className="button"> Edit </button> 
             </Link>
