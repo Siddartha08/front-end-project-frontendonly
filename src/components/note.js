a56d4a50bdae040cdf759dc598f58812b5490c26
unknown <jemery89@gmail.com>
2019-10-27 22:19:10 -0500

    slight change to id in url

--- src/components/note.js
+++ src/components/note.js
@@ -22,7 +22,7 @@ console.log(props)
             
             </h1>
             <div>{props.notes[props.match.params.id].content}</div>
-            <Link  to={`/edit/${props.notes[props.match.params.id]._id}`} >
+            <Link  to={`/edit/${props.match.params.id}`} >
             <button className="button"> Edit </button> 
             </Link>
             </div>
