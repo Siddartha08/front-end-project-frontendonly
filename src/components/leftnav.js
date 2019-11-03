d68b549e1fe87a7cb38f706059732280cad1b347
unknown <jemery89@gmail.com>
2019-11-02 21:19:25 -0500

    added text to start the heroku server

--- src/components/leftnav.js
+++ src/components/leftnav.js
@@ -9,9 +9,14 @@ export default class Leftnav extends Component {
         return(
             <div className="leftNav">
             <h1>Lambda Notes</h1>
+
+            <a href="https://notesapplambda.herokuapp.com/">click to start 
+            <br> heroku first</br>
+
             <Link to={'/'} > <button className="button" text="View Notes"> View Notes </button> </Link>
             <Link to={'/createnote'} > <button className="button" text="+ Create New Note">+ Create New Note</button> </Link>
-            <a href="https://notesapplambda.herokuapp.com/">click to start heroku</a>
+           
+            </a>
             </div>
         )
     }
