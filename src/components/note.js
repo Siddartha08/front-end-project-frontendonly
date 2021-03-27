3d81340f4193d80098821438f827d226709f5f16
Siddartha08 <jemery89@gmail.com>
2021-03-27 12:05:53 -0500

    changes to function

--- src/components/note.js
+++ src/components/note.js
@@ -3,22 +3,29 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {deleteNote} from '../actions';
 
-const deleteFunction = (props) => {
-    function handleButton(id) {
-        console.log('handlebutton working')
-        this.props.deleteNote(props.match.params.id)
-        console.log(this.props.deleteNote())
-        console.log('past delete not function')
-    }
+function handleButton(id) {
+    console.log('handlebutton working')
+    deleteNote(props.match.params.id)
+    console.log('past delete not function')
 }
 
+class Note extends Component {
 
-const Note = (props, deleteFunction) => {
-    
+    state = {
+        redirect: false,
+        title: this.props.notes[this.props.match.params.id].title,
+        content: this.props.notes[this.props.match.params.id].content,
+        id: this.props.match.params.id,
+        mdbid: this.props.notes[this.props.match.params.id]._id
+    }
     // console.log(props);
  //props.match.params.id
+ handleButton(id) {
+    console.log('handlebutton working')
+    deleteNote(props.match.params.id)
+    console.log('past delete not function')
+}
 
-        let handleButton = deleteFunction.handleButton()
     
 
         // console.log(props)
@@ -28,7 +35,7 @@ const Note = (props, deleteFunction) => {
         // const note = props.notes.filter(note => note._id == props.match.params.id)
         // console.log(note)
 
-       
+        render() {
         return (
             
             <div>
@@ -40,12 +47,12 @@ const Note = (props, deleteFunction) => {
             <button className="button"> Edit </button> 
             </Link>
             
-            <button onClick={() => {handleButton(props.match.params.id)}} className="delete"> Delete </button> 
+            <button onClick={() => {this.handleButton(props.match.params.id, this)}} className="delete"> Delete </button> 
             
             </div>
         )
         
-
+        }
 
     }
 
